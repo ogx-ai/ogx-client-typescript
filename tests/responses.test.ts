20ee58fd33025f07f2ee61dfab50ecfecfc41903
Charlie Doern <cdoern@redhat.com>
2026-04-30 15:38:56 -0400

    chore: fix CI failures and complete ogx rebranding

--- tests/responses.test.ts
+++ tests/responses.test.ts
@@ -4,7 +4,7 @@
 // This source code is licensed under the terms described in the LICENSE file in
 // the root directory of this source tree.
 
-import { getResponseOutputText } from 'llama-stack-client';
+import { getResponseOutputText } from 'ogx-client';
 import { createResponseHeaders } from 'ogx-client/core';
 import { Headers } from 'ogx-client/_shims/index';
 
